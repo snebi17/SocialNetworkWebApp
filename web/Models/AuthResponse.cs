5b0cb4559ecba614d4063aa9189f96c434ab56f3
Nejc Šneberger <nejc.snebi@gmail.com>
2023-01-04 23:28:46 +0100

    Cleaned repo

--- web/Models/AuthResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace web.Models 
-{
-    public class AuthResponse 
-    {
-        public int Id { get; set; }
-        public string Token { get; set; }
-        public DateTime ExpirationDate { get; set; }
-    }
-}
